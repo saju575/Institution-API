4b14d5ba2a308ea792c181fed9fa13ab9b157846
saju575 <sakilahmedsaju575@gmail.com>
2023-10-24 15:44:19 +0600

    students result model

---
+++ src/models/studentsResult.model.js
@@ -0,0 +1,65 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const subject = new Schema({
+  english: {
+    type: Number,
+  },
+  bangla: {
+    type: Number,
+  },
+  science: {
+    type: Number,
+  },
+  chemistry: {
+    type: Number,
+  },
+});
+
+const result = new Schema({
+  roll: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  gpa: {
+    type: Number,
+  },
+  subjects: {
+    type: [subject],
+  },
+});
+
+/* 
+    making user model schema
+*/
+const resultSchema = new Schema(
+  {
+    classTitle: {
+      type: String,
+      required: true,
+    },
+
+    session: {
+      type: String,
+      //   required: true,
+    },
+    groups: {
+      type: String,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    results: [result],
+  },
+  { timestamps: true, versionKey: false }
+);
+
+/* 
+    make a model name "User"
+*/
+const Result = model("Result", resultSchema);
+
+module.exports = Result;
