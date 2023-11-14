d11c6d5794c9d6f993461ed7f456977cb5f57a76
saju575 <sakilahmedsaju575@gmail.com>
2023-11-14 22:37:51 +0600

    student model change

--- src/models/studentsResult.model.js
+++ src/models/studentsResult.model.js
@@ -1,7 +1,37 @@
 const { Schema, model } = require("mongoose");
 
 const subject = new Schema({
-  English: {
+  "Quran Majid": {
+    type: Number,
+  },
+  "Hadith Sharif": {
+    type: Number,
+  },
+  "Hadith & Usulul Hadith": {
+    type: Number,
+  },
+  "Aqaid Fique": {
+    type: Number,
+  },
+  "Fique 1st Paper": {
+    type: Number,
+  },
+  "Fique 2nd Paper": {
+    type: Number,
+  },
+  Arabic: {
+    type: Number,
+  },
+  "Arabic 1st Paper": {
+    type: Number,
+  },
+  "Arabic 2nd Paper": {
+    type: Number,
+  },
+  "Islamic History": {
+    type: Number,
+  },
+  "Balagat & Mantiq": {
     type: Number,
   },
   Bangla: {
@@ -13,52 +43,94 @@ const subject = new Schema({
   "Bangla 2nd Paper": {
     type: Number,
   },
+  English: {
+    type: Number,
+  },
   "English 1st Paper": {
     type: Number,
   },
   "English 2nd Paper": {
     type: Number,
   },
-  Math: {
-    type: Number,
-  },
-  Religion: {
-    type: Number,
-  },
-  ICT: {
-    type: Number,
-  },
-  Physics: {
-    type: Number,
-  },
-  Biology: {
-    type: Number,
-  },
-  "Higher Math": {
-    type: Number,
-  },
-  Accounting: {
-    type: Number,
-  },
-  Finance: {
-    type: Number,
-  },
-  "Business Entrepreneurship": {
-    type: Number,
-  },
-  "Agricultural Studies": {
-    type: Number,
-  },
-  "General Science": {
-    type: Number,
-  },
-  "Bangladesh and Global Studies": {
+  "English Grammar and Composition": {
     type: Number,
   },
   Science: {
     type: Number,
   },
+  ICT: {
+    type: Number,
+  },
+  Math: {
+    type: Number,
+  },
+  "General Math": {
+    type: Number,
+  },
+  "Higher Math": {
+    type: Number,
+  },
   Chemistry: {
+    type: Number,
+  },
+  "Chemistry 1st Paper": {
+    type: Number,
+  },
+  "Chemistry 2nd Paper": {
+    type: Number,
+  },
+  Biology: {
+    type: Number,
+  },
+  "Biology 1st Paper": {
+    type: Number,
+  },
+  "Biology 2nd Paper": {
+    type: Number,
+  },
+  Physics: {
+    type: Number,
+  },
+  "Physics 1st Paper": {
+    type: Number,
+  },
+  "Physics 2nd Paper": {
+    type: Number,
+  },
+  Economics: {
+    type: Number,
+  },
+  "Economics 1st Paper": {
+    type: Number,
+  },
+  "Economics 2nd Paper": {
+    type: Number,
+  },
+  "History of Bangladesh and World Civilization": {
+    type: Number,
+  },
+  "Geography and Environment": {
+    type: Number,
+  },
+  "Agriculture Studies": {
+    type: Number,
+  },
+  "Finance And Banking": {
+    type: Number,
+  },
+  "Islam and Moral Education": {
+    type: Number,
+  },
+  "Hindu Religion and Moral Education": {
+    type: Number,
+  },
+  "Buddhist Religion and Moral Education": {
+    type: Number,
+  },
+  "Bangladesh and Global Studies": {
+    type: Number,
+  },
+  "Physical Education, Health Science and Sports": {
     type: Number,
   },
 });
